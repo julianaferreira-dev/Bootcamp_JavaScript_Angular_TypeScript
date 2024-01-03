2bdc332ff01475d72f1b919b1c57182a4c37ec99
Juliana382 <julianaferreira.sants@gmail.com>
2024-01-02 23:35:33 -0300

    Update

---
+++ Blog_Angular/src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "The Cat",
+    "description": "A beautiful cat.",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOUZMQIu9kcwmr_BXp7JLCUfQS-U2szNMuEqQj-0qqEQQtD70FA4mKTH_01q0vDIlTLto&usqp=CAU"
+  },
+  {
+    "id":"2",
+    "title": "Kitten",
+    "description": "A cute and adorable kitten.",
+    "photoCover":"https://img.freepik.com/premium-photo/beautiful-ragdoll-cat-watercolor-fantasy-clipart-illustration_962764-57650.jpg"
+  },
+  {
+    "id":"3",
+    "title": "A cat having fun",
+    "description": "A beautiful cat running.",
+    "photoCover": "https://i.etsystatic.com/42629356/r/il/89c9f5/4833645642/il_fullxfull.4833645642_q70q.jpg"
+  },
+  {
+    "id":"4",
+    "title": "The Meme",
+    "description": "Various famous memes with cats.",
+    "photoCover":"https://preview.redd.it/0u685bxa1xb61.png?auto=webp&s=58baec1084f332f19d0cd2ba5c2f0a5b9ce5a130"
+  }
+]
